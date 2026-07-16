3010dc4d79a23b1d5c811d9f0afa979c2bf3086a
nareshtharu-dev <nareshtharu.dev@gmail.com>
2026-07-16 08:36:37 +0545

    fix:prodId

--- src/pages/products/ProductDetailPage.jsx
+++ src/pages/products/ProductDetailPage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
 const ProductDetailPage = () => {
-  const { abc } = useParams();
+  const { prodId } = useParams();
   const [singleProduct, setSingleProducts] = useState();
   // fetch('https://fakestoreapi.com/products/1')
   const fetchSingleProduct = async () => {
